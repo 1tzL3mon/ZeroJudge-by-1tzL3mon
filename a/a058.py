560cad9eb99ad25d99fd6be84ccfc00088769b82
1tzL3mon <137667113+1tzL3mon@users.noreply.github.com>
2025-12-06 18:09:32 +0800

    簡單

--- a/a058.py
+++ a/a058.py
@@ -1,13 +1,10 @@
-while True:
+while True: #17行
     try:
         times = int(input())
-    except:
-        break
-    thre = 0
-    un = 0
-    du = 0
-    for t in range(times):
-        try:
+        thre = 0
+        un = 0
+        du = 0
+        for t in range(times):
             n = int(input())
             if n % 3 == 0:
                 thre +=1
@@ -15,6 +12,20 @@ while True:
                 un += 1
             else:
                 du += 1
-        except:
-            break
+    except:
+        break
     print(f"{thre} {un} {du}")
+
+
+#企鵝answer 11行
+ip = int(input())
+a, b, c = 0, 0, 0 #創建一個三物件的tuple
+for i in range(ip):
+    n = int(input())%3
+    if n == 0:
+        a += 1
+    elif n == 1:
+        b += 1
+    else:
+        c += 1
+print(a, b, c) #print tuple的時候，不會有逗點
