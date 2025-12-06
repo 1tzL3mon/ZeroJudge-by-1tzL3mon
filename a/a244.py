64c06922c3f91cf1b40805c437e4f74ce77401c8
1tzL3mon <137667113+1tzL3mon@users.noreply.github.com>
2025-12-06 15:55:09 +0800

    還算簡單

--- a/a244.py
+++ a/a244.py
@@ -1,11 +1,11 @@
-while True:
+while True: #21行
     try:
         times = int(input())
     except:
         break
     l = []
     for t in range(times):
-        l.append(input())
+        l.append(input()) #空list增加新物件要用append
     for s in range(times):
         try:
             if int(l[s].split()[0]) == 1:
@@ -19,3 +19,17 @@ while True:
         except:
             break
     break
+
+#企鵝answer 12行
+ip = int(input())
+for i in range(ip):
+    data = input().split()
+    num = int(data[0])
+    if num == 1:
+        print(int(data[1]) + int(data[2])) #每次輸入完就直接輸出
+    elif num == 2:
+        print(int(data[1]) - int(data[2]))
+    elif num == 3:
+        print(int(data[1]) * int(data[2]))
+    else:
+        print(int(int(data[1]) / int(data[2])))
