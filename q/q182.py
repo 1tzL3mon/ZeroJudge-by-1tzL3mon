663f463fe1f5946d4b0b708a897873e604f7e6c6
1tzL3mon <137667113+1tzL3mon@users.noreply.github.com>
2025-12-07 21:58:18 +0800

    普通

--- q/q182.py
+++ q/q182.py
@@ -1,17 +1,22 @@
+import copy #22行
 w = input()
-if len(w) % 2 != 0:
-    print(no)
 times = int(input())
 setotwo = list(w)
-print(setotwo)
 for t in range(times):
     n = int(input())
     if n == 0:
         for j in range(int(len(setotwo)/2)):
-            temp = setotwo[2*j]
-            setotwo[2*j] = setotwo[2*j+1]
-            setotwo[2*j+1] = temp
+                setotwo[2*j], setotwo[2*j+1] = setotwo[2*j+1], setotwo[2*j]
     elif n == 1:
-        
-        
-print(setotwo)
+        for j in range(int(len(setotwo)/2)):
+            if str(setotwo[2*j]) > str(setotwo[2*j+1]):
+                setotwo[2*j], setotwo[2*j+1] = setotwo[2*j+1], setotwo[2*j]
+    elif n == 2:
+        halves = copy.deepcopy(setotwo) #copy.deepcopy()的複製不會使用同一個記憶體
+        setotwo = []
+        for j in range(int(len(w)/2)):
+            setotwo.append(halves[j])
+            setotwo.append(halves[int(len(w)/2)+j])
+for i in range(len(setotwo)):
+    print(setotwo[i], end='')
+print()
