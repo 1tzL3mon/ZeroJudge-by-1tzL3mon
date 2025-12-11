26baa951b5bba3b065ae0bd739eb893bd26645f7
1tzL3mon <137667113+1tzL3mon@users.noreply.github.com>
2025-12-11 23:33:55 +0800

    有點複雜

--- b/b127.py
+++ b/b127.py
@@ -8,3 +8,18 @@ while True: #10行
         print(b)
     except:
         break
+
+#企鵝answer 13行
+while True:
+    try:
+        n = int(input())
+        sum = 0
+        ls = [1]
+        for i in range(1, n+1): #i從1開始
+            if i == 1:
+                ls.append(1)
+            else:
+                ls.append(ls[i-1]+ls[i-2])
+        print(ls[n])
+    except EOFError:
+        break
