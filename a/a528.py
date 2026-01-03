a6be70d490c097c658c8636edd61474cc1a43c12
1tzL3mon <137667113+1tzL3mon@users.noreply.github.com>
2026-01-03 20:19:35 +0800

    簡單

---
+++ a/a528.py
@@ -0,0 +1,11 @@
+while True:
+    try:
+        num = int(input())
+        list = []
+        for _ in range(num):
+            list.append(int(input()))
+        list.sort()
+        for i in range(num):
+            print(list[i])
+    except:
+        break
