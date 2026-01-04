91c6c87f9045c5398ff4d3e82ad5be64f9b4fc92
1tzL3mon <137667113+1tzL3mon@users.noreply.github.com>
2026-01-04 12:05:29 +0800

    普通，有我忘了的insert和pop功能

--- f/f046.py
+++ f/f046.py
@@ -11,3 +11,23 @@ if t + wid > wid + len(strin):
 for i in range(wid):
     result.append(lisstr[t+i])
 print(''.join(result))
+
+#企鵝answer
+wide = int(input())
+word = list(input())
+time = int(input())
+ls = []
+for _ in range(wide):
+    ls.append(" ")
+for item in word:
+    if time == 0:
+        break
+    else:
+        ls.pop(0) #先在最前面拿掉一個空格
+        ls.append(item)
+    time -= 1
+while time != 0:
+    ls.pop(0) #不斷拿掉最前面的，直到time變成0
+    ls.append(" ")
+    time -= 1
+print(*ls, sep="") #(*list, sep='')
