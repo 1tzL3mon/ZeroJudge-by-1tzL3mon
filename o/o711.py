270f64ca11b9c1ce9fd40cb081fa3d56ecd5ba97
1tzL3mon <137667113+1tzL3mon@users.noreply.github.com>
2026-04-07 16:05:39 +0800

    普通難

---
+++ o/o711.py
@@ -0,0 +1,31 @@
+while True:
+    try:
+        a = int(input())
+        x, y, g, h = map(int, input().split())
+        ki = list(map(int, input().split()))
+        left = (x**2) * g + (y**2) * h
+        xh = (x**2) * g
+        yh = (y**2) * h
+        et = list()
+        for i in ki:
+            if left <= 0: #append(0)找最大值時會被跳過
+                et.append(0)
+                continue
+            if i >= left:
+                if left > yh:
+                    et.append(h + (left - yh) // (x**2))
+                else:
+                    et.append(left // (y**2))
+                left = 0
+            else:
+                if left > yh:
+                    if left - i >= yh:
+                        et.append(i // (x**2))
+                    else:
+                        et.append((left - yh) // (x**2) + (i - (left - yh)) // (y**2))
+                else:
+                    et.append(i // (y**2))
+                left -= i
+        print(int(max(et)))
+    except:
+        break
