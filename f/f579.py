516d3fefcc24fdc2e3a24fc6a3ede188ce470495
1tzL3mon <137667113+1tzL3mon@users.noreply.github.com>
2026-04-10 17:44:33 +0800

    複雜，有dictionary功能

--- f/f579.py
+++ f/f579.py
@@ -1,11 +1,11 @@
-a, b = map(int, input().split())
+itema, itemb = map(int, input().split())
 n = int(input())
 res = 0
 for i in range(n):
     d = {}
-    dil = input().split()
-    for j in range(len(dil)-1):
-        d[int(dil[j])] = d.get(int(dil[j]), 0) + 1
-    if (d.get(a,0)-d.get(-a,0)) > 0 and (d.get(b,0)-d.get(-b,0)) > 0: #正數的key的value減掉負數的key的value
+    dil = list(map(int, input().split()))
+    for j in range(len(dil)-1): #跑完所有商品後再進入if句
+        d[dil[j]] = d.get(dil[j], 0) + 1 #把d字典裡的dil[j]物件設為他原本的數量加1
+    if (d.get(itema,0)-d.get(-itema,0)) > 0 and (d.get(itemb,0)-d.get(-itemb,0)) > 0: #正值的key的value減掉負值的key的value，且兩個都大於0
         res += 1
 print(res)
