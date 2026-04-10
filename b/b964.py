88e7d893ee569a60f8018f13aae32bad23565405
1tzL3mon <137667113+1tzL3mon@users.noreply.github.com>
2026-04-10 19:04:57 +0800

    普通，有用到邏輯的解法

--- b/b964.py
+++ b/b964.py
@@ -3,17 +3,17 @@ nums = sorted(map(int, input().split()))
 print(*(nums)) #中間有空格的string，而不是有逗號的list
 fail_max = -1
 pass_min = 101
-for x in nums: #本身就會由小到大
+for x in nums: #for迴圈本身就會由小到大
     if x < 60:
-        fail_max = x #直到x大於等於60停止
+        fail_max = x #x剛好大於等於60時就會停止設定fail_max
     elif x >= 60:
         if pass_min == 101: #x剛大於等於60就執行，且只執行一次
             pass_min = x
 if fail_max == -1:
-    print("best case") #x小於60的情況沒有發生
+    print("best case") #如果x小於60的情況沒有發生
 else:
     print(fail_max)
 if pass_min == 101:
-    print("worst case") #x大於等於60的情況沒有發生
+    print("worst case") #如果x大於等於60的情況沒有發生
 else:
     print(pass_min)
