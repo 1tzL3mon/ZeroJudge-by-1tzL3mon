41967da2477a30bb2424ad779a288e232dec589b
1tzL3mon <137667113+1tzL3mon@users.noreply.github.com>
2026-06-07 10:41:12 +0800

    簡單

--- 簡單/h081.py
+++ 簡單/h081.py
@@ -1,3 +1,8 @@
+# 兩個正整數n,D，下一行有n個整數
+# 第一個股票一定會買x，遇到價格y大於等於x+D時即賣出，並轉得利潤y-x
+# 若當下沒有持有股票且上一次的賣出價格為y，當遇到價格x小於等於y-D時則會買進股票x
+
+
 while True:
     try:
         t, d = map(int, input().split())
@@ -16,3 +21,23 @@ while True:
         print(fin)
     except:
         break
+
+
+# Gemini的解法，跟我的一模一樣
+n, d = map(int, input().split())
+prices = list(map(int, input().split()))
+total_profit = 0
+last_price = prices[0]
+has_stock = True
+for i in range(1, n):
+    current_price = prices[i]
+    if has_stock:
+        if current_price >= last_price + d:
+            total_profit = total_profit + (current_price - last_price)
+            last_price = current_price
+            has_stock = False
+    else:
+        if current_price <= last_price - d:
+            last_price = current_price
+            has_stock = True
+print(total_profit)
