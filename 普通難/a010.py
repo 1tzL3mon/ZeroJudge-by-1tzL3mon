e4b13c2aadfe61911fa2480ce4d0a3028075b6e9
1tzL3mon <137667113+1tzL3mon@users.noreply.github.com>
2026-06-07 08:39:52 +0800

    普通難，有一個找因數的方法

--- 普通難/a010.py
+++ 普通難/a010.py
@@ -1,3 +1,6 @@
+# 範例輸入1  20
+# 範例輸出1  2^2 * 5
+
 while True:
     try:
         n = int(input())
@@ -5,16 +8,16 @@ while True:
         d = 2
         while n > 1: #只要n還沒有被除盡
             count = 0
-            while n % d == 0: #如果n可以被d整除，便會開始執行
+            while n % d == 0: #如果n可以被d整除，便會開始除以d並增加coumt直到無法再被d整除為止
                 count += 1
                 n //= d
-            if count > 0:
+            if count > 0: #如果count大於0，代表剛才有至少除以d至少一次
                 if count == 1:
                     ans.append(str(d)) #直接把d當成文字儲存
                 else:
                     ans.append(f"{d}^{count}") #直接把d^count當成文字儲存
             d += 1
-            if d * d > n and n > 1: #如果一直到根號n都沒有找到n的因數，代表n是質數
+            if d * d > n and n > 1: #如果d一直到根號n (d^2>n) 都沒有找到n的因數，代表n是質數
                 ans.append(str(n))
                 break
         print(" * ".join(ans))
